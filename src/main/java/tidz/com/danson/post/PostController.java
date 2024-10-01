f96b76229b17e2b410bd5af58bc63289947094c4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-10-01 16:11:42 -0300

    should return ok status when returning all posts

--- src/main/java/tidz/com/danson/post/PostController.java
+++ src/main/java/tidz/com/danson/post/PostController.java
@@ -1,5 +1,18 @@
 package tidz.com.danson.post;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+	@GetMapping("")
+	List<Post> findAll() {
+		return null;
+	}
 
 }
